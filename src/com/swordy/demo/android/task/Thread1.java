4dcc3f95826788ebdabc42408176dfeb272854cd
swordy <mryangjian@live.com>
2013-10-24 14:40:03 +0800

    create a Thread sample

---
+++ src/com/swordy/demo/android/task/Thread1.java
@@ -0,0 +1,92 @@
+package com.swordy.demo.android.task;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.widget.TextView;
+
+import com.swordy.demo.android.R;
+
+public class Thread1 extends Activity
+{
+    public static final String TAG = Thread1.class.getSimpleName();
+    
+    private static final int STATE_PREPERD = 0;
+    
+    private static final int STATE_STARTED = 1;
+    
+    private static final int STATE_PAUSED = 2;
+    
+    private static final int STATE_STOPED = 3;
+    
+    private TextView mTextView;
+    
+    private int mNumber = 0;
+    
+    private int mState = STATE_PREPERD;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.thread1);
+        
+        mTextView = (TextView)findViewById(R.id.textView1);
+        mThread.start();
+    }
+    
+    public void onViewClicked(View v)
+    {
+        switch (v.getId())
+        {
+            case R.id.button1:
+                mState = STATE_STARTED;
+                break;
+            case R.id.button2:
+                mState = STATE_PAUSED;
+                break;
+            case R.id.button3:
+                mState = STATE_STOPED;
+                break;
+        }
+    }
+    
+    private Handler mHandler = new Handler(){
+
+        @Override
+        public void handleMessage(Message msg)
+        {
+            mTextView.setText("数字：" + mNumber);
+        }
+        
+    };
+    
+    private Thread mThread = new Thread()
+    {
+        
+        @Override
+        public void run()
+        {
+            while (mState != STATE_STOPED)
+            {
+                try
+                {
+                    Thread.sleep(500);
+                }
+                catch (InterruptedException e)
+                {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                if(mState == STATE_PAUSED)
+                    continue;
+                
+                mNumber++;
+                mHandler.sendEmptyMessage(0);
+            }
+        }
+        
+    };
+}
