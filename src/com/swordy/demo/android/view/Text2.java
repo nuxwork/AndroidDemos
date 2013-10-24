2fe5f186b6c3bec4c3fbcbe8e63a725f573d3fd0
swordy <mryangjian@live.com>
2013-10-24 14:24:41 +0800

    update resource id from textview1 to textView1

--- src/com/swordy/demo/android/view/Text2.java
+++ src/com/swordy/demo/android/view/Text2.java
@@ -16,7 +16,7 @@ public class Text2 extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.text2);
 
-		TextView txv1 = (TextView) findViewById(R.id.textview1);
+		TextView txv1 = (TextView) findViewById(R.id.textView1);
 		txv1.setText(Html.fromHtml("<u>使用html实现下划线样式</u>"));
 	}
 
