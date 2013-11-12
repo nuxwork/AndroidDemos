1810f17a8fe57704ff4b47fbdcf69947c8204a7b
swordy <mryangjian@live.com>
2013-11-13 01:37:27 +0800

    create a sample of custom SeekBar

--- src/com/swordy/demo/android/library/widget/SeekBar1.java
+++ src/com/swordy/demo/android/library/widget/SeekBar1.java
@@ -3,6 +3,7 @@ package com.swordy.demo.android.library.widget;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.swordy.demo.android.R;
 import com.swordy.library.android.widget.SeekBar;
 
 public class SeekBar1 extends Activity{
@@ -12,8 +13,10 @@ public class SeekBar1 extends Activity{
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		SeekBar skb = new SeekBar(this);
-		skb = null;
+		setContentView(R.layout.library_seekbar1);
+		
+		SeekBar skb = (SeekBar)findViewById(R.id.seekBar1);
+		
 	}
 	
 }
