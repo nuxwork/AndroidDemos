837a0b1289e3a83091e01a180ca049bc3dbb3436
swordy <mryangjian@live.com>
2013-11-23 16:43:13 +0800

    1. create a sample of CheckText 2. rename "res/color/text1_text_color_selector" to "res/color/text_color_selector"

---
+++ src/com/swordy/demo/android/library/widget/CheckText1.java
@@ -0,0 +1,18 @@
+package com.swordy.demo.android.library.widget;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.swordy.demo.android.R;
+
+public class CheckText1 extends Activity
+{
+    private static final String TAG = "AndroidDemos.CheckText1";
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.library_checktext1);
+    }
+}
