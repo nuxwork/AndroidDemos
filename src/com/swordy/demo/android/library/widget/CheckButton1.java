f13e708904bdbe8e66367a8a8732a3aa01216c3c
swordy <mryangjian@live.com>
2013-11-23 15:14:50 +0800

    create a sample of CheckButton

---
+++ src/com/swordy/demo/android/library/widget/CheckButton1.java
@@ -0,0 +1,17 @@
+package com.swordy.demo.android.library.widget;
+
+import com.swordy.demo.android.R.layout;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CheckButton1 extends Activity
+{
+    private static final String TAG = "AndroidDemos.CheckButton1";
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(layout.library_checkbutton1);
+    }
+}
