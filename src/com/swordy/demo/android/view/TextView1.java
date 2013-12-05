00c3a7f701f37cc1718720291f9649b8792cefd9
swordy <mryangjian@live.com>
2013-12-05 11:06:33 +0800

    rename resources

--- src/com/swordy/demo/android/view/TextView1.java
+++ src/com/swordy/demo/android/view/TextView1.java
@@ -5,15 +5,15 @@ import android.os.Bundle;
 
 import com.swordy.demo.android.R;
 
-public class Text1 extends Activity{
+public class TextView1 extends Activity{
 	
-	private static final String TAG = Text1.class.getSimpleName();
+	private static final String TAG = TextView1.class.getSimpleName();
 
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.text1);
+		setContentView(R.layout.textview1);
 	}
 
 }
--- src/com/swordy/demo/android/view/TextView2.java
+++ src/com/swordy/demo/android/view/TextView2.java
@@ -7,14 +7,14 @@ import android.widget.TextView;
 
 import com.swordy.demo.android.R;
 
-public class Text2 extends Activity {
+public class TextView2 extends Activity {
 
-	private static final String TAG = Text2.class.getSimpleName();
+	private static final String TAG = TextView2.class.getSimpleName();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.text2);
+		setContentView(R.layout.textview2);
 
 		TextView txv1 = (TextView) findViewById(R.id.textView1);
 		txv1.setText(Html.fromHtml("<u>使用html实现下划线样式</u>"));
