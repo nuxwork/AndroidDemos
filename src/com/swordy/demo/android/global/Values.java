a0bcf1c93cd7fa933e2a0698ebad6ff2af95433e
swordy <mryangjian@live.com>
2014-01-13 16:58:10 +0800

    1. removed

--- src/com/swordy/demo/android/global/Values.java
+++
@@ -1,6 +0,0 @@
-package com.swordy.demo.android.global;
-
-public class Values{
-	public static final String ACTION_SAMPLE_CODE = "com.swordy.demo.SIMPLE_CODE";
-	public static final String EXTRA_PATH = "com.swordy.demo.android.Path";
-}
